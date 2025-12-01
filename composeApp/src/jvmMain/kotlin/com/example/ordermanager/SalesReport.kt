eb0626af6b42f65bf3f3807690cb24c4e685a8c2
tommyfenske <t.fenske619@gmail.com>
2025-12-01 14:37:03 -0600

    Documented SalesReport class

--- composeApp/src/jvmMain/kotlin/com/example/ordermanager/SalesReport.kt
+++ composeApp/src/jvmMain/kotlin/com/example/ordermanager/SalesReport.kt
@@ -1,31 +1,56 @@
 package com.example.ordermanager
 
+/**
+ * Takes either an item, order, or list of orders and calculates 3 values: totalExpense, totalPrice, and totalProfit.
+ * @property totalExpense expense restaurant pays for all items in report
+ * @property totalPrice price the customer pays for all items in report
+ * @property totalProfit profit the restaurant makes for all items in report
+ * @author Tommy Fenske
+ */
 class SalesReport {
     var totalProfit: Double = 0.0;
     var totalExpense: Double = 0.0;
     var totalPrice: Double = 0.0;
 
+    /**
+     * Constructor for one item
+     * @param i Item to generate SalesReport for
+     */
     constructor(i: Item) {
         totalExpense = i.quantity * ItemMap.map.getValue(i.name);
         totalPrice = i.getTotal();
         totalProfit = totalPrice - totalExpense;
     }
 
-    constructor(o: Order) {
-        for (item: Item in o.items) {
+    /**
+     * Constructor for one order
+     * Adds together the expense, price, and profit of all items in the order.
+     * @param order Order to generate SalesReport for
+     */
+    constructor(order: Order) {
+        for (item: Item in order.items) {
             var report: SalesReport = SalesReport(item);
             addReport(report);
         }
     }
 
-    constructor(l: List<Order>) {
-        for (o: Order in l) {
-            var report: SalesReport = SalesReport(o);
+    /**
+     * Constructor for a list of orders
+     * Adds together the expense, price, and profit of all orders in the list.
+     * @param list List of orders to generate SalesReport for
+     */
+    constructor(list: List<Order>) {
+        for (order: Order in list) {
+            var report: SalesReport = SalesReport(order);
             addReport(report);
         }
     }
 
-    fun addReport(report: SalesReport) {
+    /**
+     * Adds values from a different SalesReport
+     * @param report report to add values from
+     */
+    private fun addReport(report: SalesReport) {
         this.totalExpense += report.totalExpense;
         this.totalPrice += report.totalPrice;
         this.totalProfit += report.totalProfit;
