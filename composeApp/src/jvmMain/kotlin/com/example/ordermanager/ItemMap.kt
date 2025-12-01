55e4da3b7b756b5a42cc53b435f633acf184d5d9
tommyfenske <t.fenske619@gmail.com>
2025-12-01 13:27:48 -0600

    Added ItemMap class

---
+++ composeApp/src/jvmMain/kotlin/com/example/ordermanager/ItemMap.kt
@@ -0,0 +1,12 @@
+package com.example.ordermanager
+
+class ItemMap {
+    companion object {
+        val map: HashMap<String, Double> = hashMapOf(
+            "Burger" to 2.99,
+            "Fries" to 1.99,
+            "Milkshake" to 1.49,
+            "Soda" to 0.25,
+        );
+    }
+}
