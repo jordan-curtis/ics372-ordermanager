8dcd85e4d866799e48ca10c381ad9b76f054185c
Ahmed <jjhjjff4@gmail.com>
2025-12-01 20:07:07 -0600

    created Parsers.kt (OrderParser interface + parser classes) and add ParsersTest.kt to test them using the testFiles data

---
+++ composeApp/src/jvmMain/kotlin/com/example/ordermanager/ParsersTest.kt
@@ -0,0 +1,39 @@
+package com.example.ordermanager
+
+import java.io.File
+
+//helper fun to help me print cool link :D
+fun printLine() {
+    println("=".repeat(30))
+}
+
+/*
+    *-_-*this is a small test file to check if the parsers work.*-_-*
+    *-_-*it uses the testFiles folder.*-_-*
+*/
+
+fun main() {
+
+    val jsonParser = JsonOrderParser()
+    val xmlParser = XmlOrderParser()
+
+    val folder = File("testFiles")
+
+    printLine()
+    println("*-_-*testing json parser*-_-*")
+    printLine()
+
+    val jsonOrders = jsonParser.parse(folder)
+    jsonOrders.forEachIndexed { index, order ->
+        println("from file order${index + 1}.json -> ${order.items}")
+    }
+
+    printLine()
+    println("*-_-*testing xml parser*-_-*")
+    printLine()
+
+    val xmlOrders = xmlParser.parse(folder)
+    xmlOrders.forEachIndexed { index, order ->
+        println("from file order${index + 4}.xml -> ${order.items}")
+    }
+}
