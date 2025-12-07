7edfc80f353c1ad6809b36690c3fae9e10a68d0d
tommyfenske <t.fenske619@gmail.com>
2025-12-07 17:50:06 -0600

    Added DirectoryClass, doesn't send orders to OrderManager yet

---
+++ composeApp/src/jvmMain/kotlin/com/example/ordermanager/DirectoryThread.kt
@@ -0,0 +1,51 @@
+package com.example.ordermanager
+
+
+import java.io.File
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.*
+
+object DirectoryThread {
+    var shouldRun : Boolean = true;
+     fun runThread() {
+        GlobalScope.launch {
+            withContext(Dispatchers.Default) { // this: CoroutineScope
+                // Ensure data directory is created
+                val dataDir: File = File("data");
+                if (!dataDir.exists()) { dataDir.mkdir() };
+
+                while (shouldRun) {
+                    println("Polling directory: ${dataDir.absolutePath}");
+
+                    val files: Array<File> = dataDir.listFiles();
+                    if (files.isNotEmpty()) {
+                        val jsonParser: JsonOrderParser = JsonOrderParser();
+                        val xmlParser: XmlOrderParser = XmlOrderParser();
+
+                        val orders : MutableList<Order> = mutableListOf<Order>();
+                        orders.addAll(jsonParser.parse(dataDir));
+                        orders.addAll(xmlParser.parse(dataDir));
+
+                        // Delete files after parsing
+                        for (file in files) {
+                            println("Deleting: ${file.name}");
+                            file.delete();
+                        }
+
+                        for (o in orders) {
+                            println(o.orderID);
+                        }
+                    }
+                    delay(1.seconds)
+
+                    //OrderManager.addOrder(sampleOrder);
+                }
+            }
+        }
+    }
+}
+
+suspend fun greet() {
+    println("The greet() on the thread: ${Thread.currentThread().name}");
+    delay(1.seconds);
+}
--- composeApp/src/jvmMain/kotlin/com/example/ordermanager/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/ordermanager/main.kt
@@ -1,14 +1,17 @@
 package com.example.ordermanager
 
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-
+import java.io.File
+import java.io.FileFilter
 
 fun main() = application {
+    DirectoryThread.runThread();
 
     val screenState = rememberWindowState(size = DpSize(1000.dp, 700.dp),
         position = WindowPosition(300.dp, 300.dp))
@@ -21,5 +24,12 @@ fun main() = application {
         alwaysOnTop = true
     ) {
         App()
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            // Code to run on app close
+            DirectoryThread.shouldRun = false;
+        }
     }
 }
